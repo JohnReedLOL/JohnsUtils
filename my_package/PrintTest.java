b60c8fb1665bde2f95c448a2d39955b465c3d4f2
John Reed <JohnReedLOL@ufl.edu>
2015-07-27 18:54:10 -0400

    added a variant of the origional printing/logging tool.

---
+++ my_package/PrintTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my_package;
+
+/**
+ *
+ * @author johnmichaelreed2
+ */
+public class PrintTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Print.good("lala");
+        Print.bad("nana");
+        Print.veryBad("lala1");
+        Print.slightlyBad("lala2");
+        Print.veryGood("haha");
+    }
+    
+}
