752d2b74800a8dfbe1f23302d8f667e6144e9358
John-Michael Reed <JohnReedLOL@ufl.edu>
2015-07-30 19:32:58 -0400

    Update PrintTest.java

--- my_package/PrintTest.java
+++ my_package/PrintTest.java
@@ -1,26 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package my_package;
+// Use whatever package you like.
 
 /**
- *
+ * Tests the contents of the Print class.
  * @author johnmichaelreed2
  */
-public class PrintTest {
-
-    /**
-     * @param args the command line arguments
-     */
+class PackageTest {
     public static void main(String[] args) {
-        // TODO code application logic here
-        Print.good("lala");
-        Print.bad("nana");
-        Print.veryBad("lala1");
-        Print.slightlyBad("lala2");
-        Print.veryGood("haha");
+        Print.myDefaultPrintStreamSetter(Print.DefaultPrintStream.ONLY_STANDARD_ERROR);
+        Print.exception(new Exception("I am a new exception"), "New exception detected");
+        Print.exception(new Exception("I am a new exception2"));
+        Print.bad("This is bad.");
+        Print.slightlyBad("This is slightly bad.");
+        Print.good("This is good.");
+        Print.slightlyGood("This is slightly good.");
+        Print.veryGood("This is very good.");
+        Tester.check(false);
     }
-    
 }
